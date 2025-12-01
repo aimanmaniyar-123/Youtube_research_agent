f10857ef6b5362d14d8fe3fae5582e7702f5ee4b
aimanmaniyar-123 <143692345+aimanmaniyar-123@users.noreply.github.com>
2025-12-01 14:38:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Start FastAPI backend in background
-uvicorn backend.main:app --host 0.0.0.0 --port 8000 &
+uvicorn main:app --host 0.0.0.0 --port 8000 &
 
 # Wait for backend to start
 sleep 10
